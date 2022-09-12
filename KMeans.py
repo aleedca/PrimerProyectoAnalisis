97a889d31d3cfdb735f8d44f6b8a855e0fed9413
aleedca <acerdas1701@estudiantec.cr>
2022-09-12 15:25:43 -0600

    new code kmeans

--- KMeans.py
+++ KMeans.py
@@ -1,46 +1,54 @@
-import random
-
-class KMEANS:
-
-    #constructor
-    def __init__(self, k):
-        self.MAXVECTOR = 5 #max could be 10,000
-
-        self.vector = []
-        self.centroids = []
-
-        self.K = k #must be greater than 0
-        #hacer diccionario con labels para clasificarlos
-
-    #methods
-    def generateVector(self):
-        for index in range(self.MAXVECTOR):
-            self.vector.append([random.randint(0,100),random.randint(0,100)])
-
-    def createRandomCentroids(self):
-        self.centroids.clear()
-        for index in range(self.K):
-            randomNumber = random.randint(0,self.MAXVECTOR-1)
-            self.centroids.append(self.vector[randomNumber])
-
-    def kmeans(self):
-        self.createRandomCentroids()
-        for element in self.centroids:
-            print("Centroid:", element)
-
-        print("distancias")
-
-    def printVector(self):
-        for element in self.vector:
-            print(element, end = "")
-        print()
-
-    #main
-    def main(self):
-        self.generateVector()
-        self.printVector()
-        self.kmeans()
-
-if __name__ == "__main__":
-    kmeans = KMEANS(3)
-    kmeans.main()
+#Loading the required modules
+import numpy as np
+from scipy.spatial.distance import cdist 
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+ 
+#Defining our function 
+def kmeans(x,k, no_of_iterations):
+    idx = np.random.choice(len(x), k, replace=False)
+    #Randomly choosing Centroids 
+    centroids = x[idx, :] #Step 1
+     
+    #finding the distance between centroids and all the data points
+    distances = cdist(x, centroids ,'euclidean') #Step 2
+     
+    #Centroid with the minimum Distance
+    points = np.array([np.argmin(i) for i in distances]) #Step 3
+     
+    #Repeating the above steps for a defined number of iterations
+    #Step 4
+    for _ in range(no_of_iterations): 
+        centroids = []
+        for idx in range(k):
+            #Updating Centroids by taking mean of Cluster it belongs to
+            temp_cent = x[points==idx].mean(axis=0) 
+            centroids.append(temp_cent)
+ 
+        centroids = np.vstack(centroids) #Updated Centroids 
+         
+        distances = cdist(x, centroids ,'euclidean')
+        points = np.array([np.argmin(i) for i in distances])
+         
+    return points 
+ 
+ 
+#Load Data
+data = load_digits().data
+pca = PCA(2)
+  
+#Transform the data
+df = pca.fit_transform(data)
+ 
+#Applying our function
+label = kmeans(df,10,1000)
+ 
+#Visualize the results
+ 
+u_labels = np.unique(label)
+for i in u_labels:
+    plt.scatter(df[label == i , 0] , df[label == i , 1] , label = i)
+plt.legend()
+plt.show()
