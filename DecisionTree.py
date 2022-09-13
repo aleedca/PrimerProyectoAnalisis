69f9dada289fc6a208de3a54dbc90f86ac970e44
Isaac <isaacarayabusiness@hotmail.com>
2022-09-12 18:05:42 -0600

    first running test completed

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,5 +1,7 @@
 import numpy as np
 from collections import Counter
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 
 def entropy(s):
@@ -200,3 +202,16 @@ class DecisionTree:
         '''
         # Call the _predict() function for every observation
         return [self._predict(x, self.root) for x in X]
+
+def main():
+    iris = load_iris()
+
+    X = iris['data']
+    y = iris['target']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = DecisionTree()
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
+    print(preds)
+
+main()
